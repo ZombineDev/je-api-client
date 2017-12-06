b404043a5f6b6c5f419006f8651d36478314d8e9
ZombineDev <petar.p.kirov@gmail.com>
2017-12-06 11:58:39 +0200

    Factor common sub-expressions in the test code

--- ApiClient/ConsoleTest/Program.cs
+++ ApiClient/ConsoleTest/Program.cs
@@ -15,14 +15,18 @@ namespace ConsoleTest
             var res = client.GetBasicAsync().Result;
             Console.WriteLine(res.Result.Name);
 
-            var res2 = client.Login("test-account@jarvis-edge.io", "test").Result;
+            var jeTestEmail = "test-account@jarvis-edge.io";
+            var jeTestPass = "test";
+
+            var res2 = client.Login(jeTestEmail, jeTestPass).Result;
             Console.WriteLine(res2.Result.Token);
 
             var guid = Guid.NewGuid().ToString();
-            var userName = $"user1_{guid}";
+            var pseudo = $"user1_{guid}";
             var email = $"test{guid}@mail.com";
+            var pass = "Sup3rSecurePasswd!";
 
-            var res3 = client.Register(userName, "first", "last", email, "Sup3rSecurePasswd!").Result;
+            var res3 = client.Register(pseudo, "first", "last", email, pass).Result;
             Console.WriteLine(res3.Result.Token);
 
             var res4 = client.RequestNewPassword(email).Result;
