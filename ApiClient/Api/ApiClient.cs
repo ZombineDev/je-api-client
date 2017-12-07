adf5da20549a615e58209a90fb097a985c5526a1
ZombineDev <petar.p.kirov@gmail.com>
2017-12-07 14:27:46 +0200

    Remove Api.ModifyUser.Avatar property
    It's not part of the actual schema.

--- ApiClient/Api/ApiClient.cs
+++ ApiClient/Api/ApiClient.cs
@@ -100,7 +100,6 @@ namespace Api
     public class ModifyUser
     {
         public string Pseudo { get; set; }
-        public string Avatar { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
